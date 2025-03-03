18433c7da18587e1e01bc9c6a56df13a9aab9589
Elif <vuralelif@hotmail.com.tr>
2025-03-03 19:59:35 +0000

    Renamed routes and added cakes screen route - "get"

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\DeveloperController;
-use App\Http\Controllers\Controller;
 
 
 
-Route::get('/uploadForm', [DeveloperController::class, 'showUploadForm']);
+Route::get('/', [DeveloperController::class, 'showUploadForm']);
 Route::post('/upload-birthdays', [DeveloperController::class, 'uploadBirthdays']);
 Route::get('/cake-days', [DeveloperController::class, 'getCakeDays']);
+Route::get('/cakes', [DeveloperController::class, 'showCakes']);
+
+
