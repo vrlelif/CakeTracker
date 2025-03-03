b2e05b1636d8e70a296beaa8b77ea4467e52175f
Elif <vuralelif@hotmail.com.tr>
2025-03-03 18:47:55 +0000

    uploadBirthdays and shoeUplaodForm added

--- app/Http/Controllers/DeveloperController.php
+++ app/Http/Controllers/DeveloperController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+
 use App\Models\Developer;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
@@ -11,20 +13,39 @@ use App\Providers\CakeDayProvider;
 
 class DeveloperController extends Controller
 {
+
+    public function showUploadForm(): View
+    {
+        return view('uploadForm');
+    }
     public function uploadBirthdays(Request $request)
     {
-        $request->validate(['file' => 'required|mimes:txt']);
+        $request->validate([
+            'file' => 'required|file|mimetypes:text/plain,text/csv,text/tsv'
+        ]);
 
         $file = $request->file('file');
         $content = file($file->getPathname());
 
         foreach ($content as $line) {
-            [$name, $dob] = explode(',', trim($line));
-            Developer::create(['name' => $name, 'date_of_birth' => $dob]);
+            $data = explode(',', trim($line));
+
+            if (count($data) !== 2) {
+                continue; // Skip invalid lines
+            }
+
+            [$name, $dob] = $data;
+
+            if (!preg_match('/\d{4}-\d{2}-\d{2}/', $dob)) {
+                continue; // Skip invalid date format
+            }
+
+            Developer::updateOrCreate(['name' => $name], ['date_of_birth' => $dob]);
         }
 
-        return response()->json(['message' => 'File processed successfully'], 200);
+        return redirect('/uploadForm')->with('success', 'File uploaded successfully!');
     }
+
 
 
     public function getCakeDays(CakeDayProvider $service)
