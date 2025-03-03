9f29cfd6db1120ca4f2c2d0978d3994de5e0e3ea
Elif <vuralelif@hotmail.com.tr>
2025-03-03 08:47:46 +0000

    route added to "get" cake days

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeveloperController;
 use App\Http\Controllers\Controller;
 Route::post('/upload-birthdays', [DeveloperController::class, 'uploadBirthdays']);
+Route::get('/cake-days', [DeveloperController::class, 'getCakeDays']);
