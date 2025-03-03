5ea9057894d7da5d21169e5aff3164c10d96f5ac
Elif <vuralelif@hotmail.com.tr>
2025-03-03 18:49:05 +0000

    route for form added - "GET"

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\DeveloperController;
 use App\Http\Controllers\Controller;
+
+
+
+Route::get('/uploadForm', [DeveloperController::class, 'showUploadForm']);
 Route::post('/upload-birthdays', [DeveloperController::class, 'uploadBirthdays']);
 Route::get('/cake-days', [DeveloperController::class, 'getCakeDays']);
